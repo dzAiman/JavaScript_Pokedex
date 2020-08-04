6071531795d21075db5d44d3dee936a76e275e97
dzAiman <dzaidev@gmail.com>
2020-08-05 00:06:52 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const pokemons = document.getElementById('pokemons');
+
+const fetchPokemon = () => {
+    const promises = [];
+    for( let i = 1; i <= 150; i++){
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+
+        promises.push(fetch(url).then((res) => res.json()));
+           
+    }
+
+    Promise.all(promises).then(results => {
+        const pokemon = results.map((data) => ({
+            name : data.name,
+                id : data.id,
+                image : data.sprites["front_default"],
+                type : data.types.map((type) => type.type.name).join(', ')
+        }));
+
+        displayPokemon(pokemon);
+    
+    });
+            
+            
+}
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon.map ((poke) => 
+        `
+            <li class = "card">
+                <img class = "card-image" src = "${poke.image}" />
+                <h2 class = "card-title"> ${poke.id}. ${poke.name} </h2>
+                <p class = "card-sub"> ${poke.type} </p>
+            </li>
+        `
+        ).join('');
+        pokemons.innerHTML = pokemonHTMLString;
+};
+
+
+fetchPokemon();
